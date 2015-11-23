ccfdd7a8bc47b9290d43f9468ab1439d8dc0863f
钱海龙 <qianhailong@u51.com>
2015-11-23 16:59:59 +0800

    add custom widget support

--- app/src/main/java/com/bupt/enniu/indicatorcanvasdemo/CustomIndicatorView.java
+++ app/src/main/java/com/bupt/enniu/indicatorcanvasdemo/CustomIndicatorView.java
@@ -1,6 +1,7 @@
 package com.bupt.enniu.indicatorcanvasdemo;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
@@ -23,36 +24,65 @@ public class CustomIndicatorView extends View {
     int changeX = 0;  //根据手指滑动的距离而计算出的指示点该移动的距离
     int downStartX;  //手指按下时的坐标
     int index=0;  //当前指示点的位置,从0开始
-    int count =5;  //指示点的个数,默认是5
+    int Indicator_Count =5;  //指示点的个数,默认是5
+
+    int Indicator_color; //指示点的颜色
+    int Indicator_background_color;  //背景指示点的颜色
+    int Indicator_radius;  //指示点的半径
+
+    OnIndicatorChangeListener listener;
+
+    int DisplayWidth; //屏幕的宽度
 
     public CustomIndicatorView(Context context){
         super(context);
-        paintGray = new Paint();
-        paintGray.setColor(Color.GRAY);
-        paintGray.setStrokeWidth(5);
-        paintGray.setAntiAlias(true);
-
-        paintRed = new Paint();
-        paintRed.setColor(Color.RED);
-        paintRed.setStrokeWidth(5);
-        paintRed.setAntiAlias(true);
-
-        this.mContext =  context;
+        init();
     }
 
-    public CustomIndicatorView(Context context, int count){
-        this(context);
-        this.count = count;
+    public CustomIndicatorView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
     }
 
     public CustomIndicatorView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        this.mContext =  context;
+        TypedArray typedArray = mContext.getTheme()
+                .obtainStyledAttributes(attrs,
+                        R.styleable.CustomIndicatorView,
+                        0,
+                        0);
+
+        try{
+            Indicator_Count = typedArray.getInt(R.styleable.CustomIndicatorView_indicator_count,5);
+            Indicator_radius = (int) typedArray.getDimension(R.styleable.CustomIndicatorView_indicator_radius,10.0f);
+            Indicator_color = typedArray.getColor(R.styleable.CustomIndicatorView_indicator_color,Color.RED);
+            Indicator_background_color = typedArray.getColor(R.styleable.CustomIndicatorView_indicator_background_color,Color.GRAY);
+
+        }finally {
+            typedArray.recycle();
+        }
+
+        init();
+
+    }
+
+    void init(){
+        paintGray = new Paint();
+        paintGray.setColor(Indicator_background_color);
+        paintGray.setStrokeWidth(5);
+        paintGray.setAntiAlias(true);
+
+        paintRed = new Paint();
+        paintRed.setColor(Indicator_color);
+        paintRed.setStrokeWidth(5);
+        paintRed.setAntiAlias(true);
+
+        DisplayWidth = mContext.getResources().getDisplayMetrics().widthPixels;
     }
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-        int finalWidth = 0,finalHeight = 0;
+        int finalWidth,finalHeight;
 
         int widthSpecSize = MeasureSpec.getSize(widthMeasureSpec);
         int widthSpecMode = MeasureSpec.getMode(widthMeasureSpec);
@@ -60,34 +90,40 @@ public class CustomIndicatorView extends View {
         int heightSpecMode = MeasureSpec.getMode(heightMeasureSpec);
         if(widthSpecMode == MeasureSpec.EXACTLY){
             finalWidth = widthSpecSize;
-        }else if(widthSpecMode == MeasureSpec.AT_MOST){
+        }else {
             finalWidth = mContext.getResources().getDisplayMetrics().widthPixels/5; //屏幕宽度的1/5
         }
 
         if(heightSpecMode == MeasureSpec.EXACTLY){
             finalHeight = heightSpecSize;
-        }else if(widthSpecMode == MeasureSpec.AT_MOST){
-            finalHeight = 14;
+
+            //如果控件的高度小于指示点的直径,那么就把高度设置为指示点的直径
+            if(finalHeight < Indicator_radius*2){
+                finalHeight = Indicator_radius*2;
+            }
+        }else {
+            finalHeight = Indicator_radius*2;  //指示点的直径
         }
 
-        setMeasuredDimension(finalWidth,finalHeight);
+        setMeasuredDimension(finalWidth, finalHeight);
 
     }
+
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        width = canvas.getWidth();
-        int height = canvas.getHeight();
-        startX = width*2/5;  //从屏幕的2/5处到3/5处为绘制指示点的区域
-        int endX = width*3/5;
-        spaceX = width/(5*count);  //指示点的间距
-        int startY = height/5;  //指示点的纵坐标
-        for(int i=0;i<count;i++){
-            canvas.drawCircle(startX+i*spaceX,startY,7,paintGray); //绘制背景指示点
+        width = getMeasuredWidth();
+        int height = getMeasuredHeight();
+        startX = Indicator_radius;  //从指示点的半径处开始
+        int endX = width-Indicator_radius; //结束位置
+        spaceX = (width-2*Indicator_radius)/(Indicator_Count-1);  //指示点的间距
+        int startY = height/2;  //指示点的纵坐标,从控件的中间位置开始绘制
+        for(int i=0;i< Indicator_Count;i++){
+            canvas.drawCircle(startX+i*spaceX,startY,Indicator_radius,paintGray); //绘制背景指示点
         }
 
-        canvas.drawCircle(startX+changeX+index*spaceX,startY,7, paintRed);  //绘制当前指示点
+        canvas.drawCircle(startX + changeX + index * spaceX, startY, Indicator_radius, paintRed);  //绘制当前指示点
 
     }
 
@@ -95,21 +131,28 @@ public class CustomIndicatorView extends View {
     public boolean onTouchEvent(MotionEvent event) {
         switch (event.getAction()){
             case MotionEvent.ACTION_DOWN:
-                downStartX = (int) event.getX();
+                downStartX = (int) event.getRawX(); //getX(),相对于空间的坐标;getRawX(),相对与屏幕的坐标
                 break;
             case MotionEvent.ACTION_MOVE:
-                changeX =(int) (event.getX()-downStartX)*spaceX/(width); //按比例计算指示点应该移动的距离,默认手指移动一屏,指示点移动一格
+                changeX =(int) (event.getRawX()-downStartX)*spaceX/(DisplayWidth); //按比例计算指示点应该移动的距离,默认手指移动一屏,指示点移动一格
                 break;
             case MotionEvent.ACTION_UP:
                 //当手指移动了超过半屏的距离,就默认过度到下一个/上一个位置
-                if((event.getX()-downStartX) > width/2){
-                    index = (index+1)%count;
-                }else if((event.getX()-downStartX) < -width/2){
-                    index = (index-1)%count;
+                if((event.getX()-downStartX) > DisplayWidth/2){
+                    index = (index+1)% Indicator_Count;
+
+                    //添加回调
+                    addIndicatorChangeListner(index);
+                }else if((event.getX()-downStartX) < -DisplayWidth/2){
+                    index = (index-1)% Indicator_Count;
                     if(index ==-1){
-                        index = count-1;
+                        index = Indicator_Count -1;
                     }
+
+                    //添加回调
+                    addIndicatorChangeListner(index);
                 }
+
                 changeX = 0; //重置
                 break;
             default:
@@ -131,5 +174,24 @@ public class CustomIndicatorView extends View {
     public void setIndex(int index){
         this.index = index;
         invalidate();
+
+        //添加回调
+        addIndicatorChangeListner(index);
+    }
+
+    //设置指示点变化时的监听
+    public void setOnIndicatorChangeListener(OnIndicatorChangeListener listener){
+        this.listener = listener;
+    }
+
+    //回调
+    public void addIndicatorChangeListner(int currectPosition) {
+        if (listener != null){
+            listener.onChangde(currectPosition);
+        }
+    }
+
+    public interface OnIndicatorChangeListener{
+        public void onChangde(int currectPosition);
     }
 }
--- app/src/main/java/com/bupt/enniu/indicatorcanvasdemo/MainActivity.java
+++ app/src/main/java/com/bupt/enniu/indicatorcanvasdemo/MainActivity.java
@@ -2,15 +2,30 @@ package com.bupt.enniu.indicatorcanvasdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
 
 public class MainActivity extends AppCompatActivity {
 
+    CustomIndicatorView customIndicatorView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
 
-        setContentView(new CustomIndicatorView(MainActivity.this));
+        customIndicatorView = (CustomIndicatorView)findViewById(R.id.customindicator);
+        //设置监听函数
+        customIndicatorView.setOnIndicatorChangeListener(new CustomIndicatorView.OnIndicatorChangeListener() {
+            @Override
+            public void onChangde(int currentPosition) {
+                Log.d("current postion",String.valueOf(currentPosition));
+            }
+        });
+    }
 
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        customIndicatorView.onTouchEvent(event);
+        return super.onTouchEvent(event);
     }
 }
